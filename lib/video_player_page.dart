91cd1e74d5684d99c273b94659fcaa18288a4c32
jishaofeng89 <1195957057@qq.com>
2020-06-06 21:34:23 +0800

    play ok

--- lib/video_player_page.dart
+++ lib/video_player_page.dart
@@ -14,6 +14,8 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
 
   VideoPlayerController _videoPlayerController;
 
+  bool _isPlaying = false;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -32,17 +34,39 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       body: _videoPlayerController.value.initialized ? 
         AspectRatio(
           aspectRatio: _videoPlayerController.value.aspectRatio,
-          child: VideoPlayer(
-            _videoPlayerController
-          ) ,
+          child: Stack(
+            children: <Widget>[
+              VideoPlayer(
+                _videoPlayerController
+              ),
+              Positioned(
+                bottom: 0,
+                child: GestureDetector(
+                  child: Icon(
+                    _isPlaying ? Icons.pause : Icons.play_arrow
+                  ),
+                  onTap: () {
+                    if(_isPlaying) {
+                      _videoPlayerController.pause();
+                    } else {
+                      _videoPlayerController.play();
+                    }
+                    setState(() {
+                      _isPlaying = !_isPlaying;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
         )
         : Container(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.play_arrow),
-        onPressed: () {
-          _videoPlayerController.play();
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.play_arrow),
+      //   onPressed: () {
+      //     _videoPlayerController.play();
+      //   },
+      // ),
     );
   }
 }
