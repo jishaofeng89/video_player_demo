9345807a8f0a0ccfceaae8bfa863ba1fc2273165
jishaofeng89 <1195957057@qq.com>
2020-10-05 19:15:33 +0800

    optimize

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-  final String url = "http://v.360inhands.com/video1.mp4";
+  final String url = "https://v.360inhands.com/%E3%80%90iOS%E4%BB%8E%E5%85%A5%E9%97%A8%E5%88%B0%E6%94%BE%E5%BC%83%E3%80%91%E7%AC%AC%E4%B8%83%E5%9B%9E%20%E8%8E%B7%E5%8F%96%E6%96%87%E4%BB%B6%E8%A6%81%E4%BB%8E%E7%BD%91%E7%BB%9C%E4%B8%8B%E8%BD%BD%EF%BC%8C%E5%B1%95%E7%8E%B0%E6%95%B0%E6%8D%AE%E9%9C%80%E8%A6%81%E6%9C%AC%E5%9C%B0%E8%A7%A3%E6%9E%90%20-%201.1%28Av6497757%2CP1%29.mp4";
 
   @override
   Widget build(BuildContext context) {
